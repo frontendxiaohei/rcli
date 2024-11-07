816e38772ab06e47a4a595329946c0a4b9bd39f1
big whiter <38613864+frontendxiaohei@users.noreply.github.com>
2024-11-07 13:19:24 +0800

    feat: csv 2 json common

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+// rcli csv -i input.csv -o output.json --header -d ','
+
+use clap::Parser;
+use rcli::{process_csv, process_genpass, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
+        }
+        SubCommand::GenPass(opts) => {
+            process_genpass(
+                opts.length,
+                opts.uppercase,
+                opts.lowercase,
+                opts.number,
+                opts.symbol,
+            )?;
+        }
+    }
+
+    Ok(())
 }
